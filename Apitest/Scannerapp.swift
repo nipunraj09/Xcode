04e296d721cae5aaa76dbeb8767f27ea33c07b57
Nipun raj <nipunraj09@gmail.com>
2025-04-21 13:28:51 +0530

    have added initial code for Xcode file

---
+++ Apitest/Scannerapp.swift
@@ -0,0 +1,24 @@
+//
+//  Scannerapp.swift
+//  Apitest
+//
+//  Created by pc on 21/04/25.
+//
+
+import SwiftUI
+
+
+
+struct Scannerapp:View{
+    
+    var body: some View{
+        Text("Hello dodge")
+        
+        
+    }
+    
+}
+
+#Preview {
+    Scannerapp()
+}
